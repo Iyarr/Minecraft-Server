f00dfbe4aca0528e841ddbd7861d27f0a70a4623
Iyarr <8.24ridiculous@gmail.com>
2023-11-25 17:06:55 +0900

    hostsファイルへのsedコマンドの修正

--- init.sh
+++ init.sh
@@ -2,17 +2,14 @@
 
 ssh-keygen -t rsa -N "" -m PEM -f "$SSH_KEY_PATH/ec2_keypair.pem"
 
-# テキスト内のすべての / を \/ にエスケープする
-# (置換前と後の文字列の/と\でもそれぞれ\をつけてエスケープされている)
-# sed 's/ \/ / \\\/ /g'
-# 2つのバックスラッシュ \\ は、正規表現内のバックスラッシュ自体をエスケープする
-escaped_ssh_key_path=$(echo "$SSH_KEY_PATH" | sed 's/\//\\\//g')
-sed -i "8s/.*/ansible_ssh_private_key_file=${escaped_ssh_key_path}\/ec2_keypair.pem/" /home/ansible/hosts
+# テキスト内のすべての /や\ を/ を前においてエスケープする
+escaped_ssh_key_path=$(echo "$SSH_KEY_PATH/ec2_keypair.pem" | sed 's/\//\\\//g')
+sed -i "s/\\(ansible_ssh_private_key_file\\)=.*/\\1=${escaped_ssh_key_path}/" /home/ansible/hosts
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set region "$region"
 
-/home/tf/script.sh start
+#/home/tf/script.sh start
 
 tail -f /dev/null
